8be8f7c411d25841f82c9cb1df4d2a4c49fe5a37
NekoZer0158 <ichuplygin@bk.ru>
2024-12-10 19:13:27 +0300

    Renaming

--- NZ_projectiles/Scripts/Free_projectiles.gd
+++ NZ_projectiles/Scripts/Free_projectiles.gd
@@ -6,3 +6,5 @@ func _on_pressed() -> void:
 	for i in get_tree().get_nodes_in_group("Projectiles_extended"):
 		if i != dont_remove_this:
 			i.queue_free()
+	for i in get_tree().get_nodes_in_group("Projectiles"):
+		i.queue_free()
