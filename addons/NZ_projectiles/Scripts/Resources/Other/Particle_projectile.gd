5b3ac475847ef09809d784bf6fa15b70ec0332ac
NekoZer0158 <ichuplygin@bk.ru>
2024-12-10 18:54:11 +0300

    Particle update

--- addons/NZ_projectiles/Scripts/Resources/Other/Particle_projectile.gd
+++ addons/NZ_projectiles/Scripts/Resources/Other/Particle_projectile.gd
@@ -3,6 +3,7 @@ class_name Particle_projectile
 extends Resource
 
 @export var particle_scene : PackedScene
+@export var make_particle_look_backwards : bool = true
 
 func spawn_particle(projectile:Projectile,add_to_this_node:Node) -> void:
 	var spawn_this_particle := particle_scene.instantiate()
@@ -10,6 +11,7 @@ func spawn_particle(projectile:Projectile,add_to_this_node:Node) -> void:
 		spawn_this_particle.global_position = projectile.global_position
 		spawn_this_particle.emitting = true
 		add_to_this_node.add_child(spawn_this_particle)
-		spawn_this_particle.look_at(projectile.global_position-projectile.speed*projectile.transform.x)
+		if make_particle_look_backwards:
+			spawn_this_particle.look_at(projectile.global_position-projectile.speed*projectile.transform.x)
 	else:
 		push_error("Wrong scene")
